878208967f5824014889e5a5ca9c41bb1caa2df2
chiz4r0 <rezky@atmaluhur.ac.id>
2025-08-07 09:09:57 +0700

    first commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    # Channel utama (stable)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... } @ inputs:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
+    unstablePkgs = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
+    lib = nixpkgs.lib;
+  in
+  {
+    nixosConfigurations = {
+      cat-linux = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+        specialArgs = {
+          inherit self inputs unstablePkgs;
+        };
+      };
+    };
+  };
+}
